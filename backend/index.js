06bcb42b6c6fdcdf02a1c84d893797b0d3967621
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-14 17:15:45 -0300

    Add css

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
-const { Server } = require("socket.io");
+const {
+    Server
+} = require("socket.io");
 
 app.use(cors());
 
@@ -20,11 +22,14 @@ io.on('connection', (socket) => {
         socket.join(data);
         console.log(`User with ID: ${socket.id} joined room: ${data}`);
     });
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit('receive_message', data);
+        console.log(`User with ID: ${socket.id} sent message: ${data.message} to room: ${data.room}`);
+    });
     socket.on('disconnect', () => {
         console.log(`User disconnected: ${socket.id}`);
     });
 });
-
 server.listen(3002, () => {
     console.log('listening on *:3009');
 });
--- chat-anime/src/chat.js
+++ chat-anime/src/chat.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+function Chat({socket, userName, roomID}) {
+    const [currentMessage, setCurrentMessage] = useState('');
+    const sendMessage = async () => {
+        if(currentMessage !== ''){
+            const messsagedata = {
+                room: roomID,
+                author: userName,
+                message: currentMessage,
+                time : new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes()
+            };
+            await socket.emit('send_message', messsagedata);
+        }
 
-function chat({socket, userName, roomID}) {
+    }
+    useEffect(() => {
+        socket.on('receive_message', (data) => {
+            console.log(data);
+        });
+    }, [socket]);
+
   return (
-    <div className='Chat'>
-
+    <div >
+    <div className='Chat__Header'>
+        <p>Anime Chat</p>
+    </div>
+    <div ></div>
+    <div >
+        <input type='text' placeholder='Type a message...' 
+           onChange={(e) =>  setCurrentMessage(e.target.value)}/>
+        <button onClick={sendMessage}>
+            &#10148;
+        </button>
+    </div>
     </div>
   )
 }
 
-export default chat
+export default Chat
