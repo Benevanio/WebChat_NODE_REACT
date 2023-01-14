47e284079903b4d2f7878fcd4738b1d72d98c428
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-14 13:44:21 -0300

    Restart server and config connection

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,25 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
-const {Server} = require("socket.io")
+const { Server } = require("socket.io");
 app.use(cors());
+
 const server = http.createServer(app);
-const Io = new Server(server,{
-    cors:{
-        origin:"http://localhost:3001",
-        methods:["GET","POST"]
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3009",
+        methods: ["GET", "POST"]
     }
 });
-Io.on('connection', (socket) => {
+io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg);
+        io.emit('chat message', msg + ' from server');
     });
 });
-server.listen(3001, () => {
-    console.log('Server running on port 3001');
+
+server.listen(3009, () => {
+    console.log('listening on *:3009');
 });
