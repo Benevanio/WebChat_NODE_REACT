28772fc2f98da7ff442fd6f73825a44bb2a5cd8b
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-14 15:57:15 -0300

    Add useState

--- chat-anime/src/App.js
+++ chat-anime/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import io from 'socket.io-client';
-
+import { useState } from 'react';
 const socket = io.connect('http://localhost:3002');
 function App() {
+const [userName, setUserName] = useState('');
+const [roomID, setRoomID] = useState('');
 
+const joinRoom = () => {
+
+}
   return (
     <div className="App">
-     <h1>Chat App</h1>
+     <h3>
+      Join the chat
+     </h3>
+    <input type="text" placeholder="Join..."onChange={(e) => setUserName(e.target.value)}/>
+    <input type="submit" value="Room ID..." onChange={(e) =>
+    setRoomID(e.target.value)}/>
+    <button type="submit">Join</button>
     </div>
   );
 }
