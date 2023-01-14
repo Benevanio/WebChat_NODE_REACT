c16220925bec21ae16af39b64f293b0028aca9e1
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-14 16:08:27 -0300

    Continue Socket.io conection

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,10 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
+    socket.on('join_room', (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
     socket.on('disconnect', () => {
         console.log(`User disconnected: ${socket.id}`);
     });
--- chat-anime/src/App.js
+++ chat-anime/src/App.js
@@ -7,7 +7,11 @@ const [userName, setUserName] = useState('');
 const [roomID, setRoomID] = useState('');
 
 const joinRoom = () => {
-
+  if(userName !== '' && roomID !== ''){
+    socket.emit('join_room', roomID);
+  }else {
+    alert('Please enter a username and room ID');
+  }
 }
   return (
     <div className="App">
@@ -15,9 +19,8 @@ const joinRoom = () => {
       Join the chat
      </h3>
     <input type="text" placeholder="Join..."onChange={(e) => setUserName(e.target.value)}/>
-    <input type="submit" value="Room ID..." onChange={(e) =>
-    setRoomID(e.target.value)}/>
-    <button type="submit">Join</button>
+    <input type="text" placeholder="Rom..."onChange={(e) => setRoomID(e.target.value)}/>
+    <button type="submit" onClick={joinRoom}>Join</button>
     </div>
   );
 }
