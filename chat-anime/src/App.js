ec0d6e9ea07127b805162a9a9ef211c8fedfe045
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-14 13:46:29 -0300

    Change directory

---
+++ chat-anime/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import io from 'socket.io-client';
+
+const socketURL = 'http://localhost:3009';
+socketURL.connect(socketURL);
+
+function App() {
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
